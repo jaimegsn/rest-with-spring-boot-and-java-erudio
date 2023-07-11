22fb6b48d535a822566feb989b575c5ed7bc81c8
jaimegsn <jaime.jgsn2@gmail.com>
2023-07-11 19:28:04 -0300

    Custom JSON serialization

---
+++ 08_CustomSerialization/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -0,0 +1,53 @@
+package br.com.erudio.controllers;
+
+import br.com.erudio.dto.v1.PersonDTO;
+import br.com.erudio.models.Person;
+import br.com.erudio.services.PersonService;
+import jakarta.websocket.server.PathParam;
+import org.apache.coyote.Response;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/person")
+public class PersonController {
+
+    PersonService personService;
+
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<PersonDTO> findAll(){
+        return personService.findAll();
+    }
+
+    @GetMapping(value = "/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public PersonDTO findById(@PathVariable(value="id") Long id){
+        return personService.findById(id);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+                produces = MediaType.APPLICATION_JSON_VALUE)
+    public PersonDTO save(@RequestBody PersonDTO person){
+        return personService.save(person);
+    }
+
+    @PutMapping(value = "/{id}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public PersonDTO update(@PathVariable(value="id") Long id, @RequestBody PersonDTO person){
+        return personService.update(person, id);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value="id") Long id){
+        personService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ 08_CustomSerialization/src/main/java/br/com/erudio/dto/v1/PersonDTO.java
@@ -0,0 +1,68 @@
+package br.com.erudio.dto.v1;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@JsonPropertyOrder({"address","first_name","last_name","gender"})
+public class PersonDTO implements Serializable {
+    public PersonDTO() {
+    }
+
+    @JsonProperty("first_name")
+    private String firstName;
+    @JsonProperty("last_name")
+    private String lastName;
+    private String address;
+    @JsonIgnore
+    private String gender;
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PersonDTO personDTO = (PersonDTO) o;
+        return Objects.equals(firstName, personDTO.firstName) && Objects.equals(lastName, personDTO.lastName) && Objects.equals(address, personDTO.address) && Objects.equals(gender, personDTO.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, address, gender);
+    }
+}
