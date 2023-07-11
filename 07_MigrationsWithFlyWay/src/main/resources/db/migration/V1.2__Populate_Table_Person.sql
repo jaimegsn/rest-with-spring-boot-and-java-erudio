2f5e531578699b2cb0ce754e4fe857d57ddfdf95
jaimegsn <jaime.jgsn2@gmail.com>
2023-07-11 12:14:27 -0300

    Adding migrations support with Flyway

---
+++ 07_MigrationsWithFlyWay/src/main/resources/db/migration/V1.2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO "person" ("id", "address", "first_name", "gender", "last_name") VALUES
+(1, 'Some address in Brazil', 'Jaime', 'Male', 'Gouveia de Souza Neto'),
+(5, 'Ceara', 'Erik', 'Male', 'Vozao'),
+(6, 'Some address in Miami', 'Lionel', 'Male', 'Messi'),
+(7, 'Some address in Madrid', 'Vinicius', 'Male', 'Junior'),
+(8, 'Some address in Barcelona', 'Lewandowski', 'Male', 'Lewa');
